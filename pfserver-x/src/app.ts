d8c4a12885937558c26bccdddea98e981833f833
vancao <caobo1712000@gmail.com>
2018-12-21 08:41:37 +0700

    delete all

--- pfserver-x/src/app.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-
-
-
-const app = express()
-
-module.exports.app = app
-export default app
--- pfserver-x/src/server.ts
+++
@@ -1,5 +0,0 @@
-import app from './app'
-
-app.listen('5000',()=>{
-    console.log('server is running ADDUU')
-})
